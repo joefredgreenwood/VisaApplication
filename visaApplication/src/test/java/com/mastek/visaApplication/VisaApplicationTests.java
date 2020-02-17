8952a50e00a1cfb807d6b280b8c77ee7beb8f116
shujaha61290 <shujaha61290@GB-LD1LPW10G007.mastek.com>
2020-02-17 13:05:01 +0000

    added Getters and Setters to Personal and ID

--- visaApplication/src/test/java/com/mastek/visaApplication/VisaApplicationTests.java
+++ visaApplication/src/test/java/com/mastek/visaApplication/VisaApplicationTests.java
@@ -3,11 +3,17 @@ package com.mastek.visaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mastek.visaApplication.entities.PersonalDetails;
+
 @SpringBootTest
 class VisaApplicationTests {
 
-	@Test
+	@Test // test add applicant details 
 	void testAddApplicant() {
+	PersonalDetails appd = new PersonalDetails();
+	appd.setPassportNo(1111111111);
+	appd.setContactLanguage("English");
+	appd.
 	}
 
 }
