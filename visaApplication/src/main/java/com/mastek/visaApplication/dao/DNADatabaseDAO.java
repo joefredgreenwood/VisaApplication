ecd79750f5220535dba450d4bdbcf87fbe9a778d
joseph61288 <joseph61288@GB-LD1LPW10G019.mastek.com>
2020-02-17 11:55:01 +0000

    DNA dao push

--- visaApplication/src/main/java/com/mastek/visaApplication/dao/DNADatabaseDAO.java
+++ visaApplication/src/main/java/com/mastek/visaApplication/dao/DNADatabaseDAO.java
@@ -1,5 +1,17 @@
 package com.mastek.visaApplication.dao;
 
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public interface DNADatabaseDAO {
 
+	
+BufferedReader=File DNA = new File(null);
 }
