954062233348e8d03c87f80ee3339d555d05dede
shujaha61290 <shujaha61290@GB-LD1LPW10G007.mastek.com>
2020-02-17 14:16:54 +0000

    added testcase

--- visaApplication/src/main/java/com/mastek/visaApplication/dao/PersonalDetailsDAO.java
+++ visaApplication/src/main/java/com/mastek/visaApplication/dao/PersonalDetailsDAO.java
@@ -1,9 +1,11 @@
 package com.mastek.visaApplication.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mastek.visaApplication.entities.PersonalDetails;
 
+@Repository
 public interface PersonalDetailsDAO extends
 										CrudRepository<PersonalDetails, Double>{
 
--- visaApplication/src/test/java/com/mastek/visaApplication/VisaApplicationTests.java
+++ visaApplication/src/test/java/com/mastek/visaApplication/VisaApplicationTests.java
@@ -12,10 +12,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mastek.visaApplication.dao.DNADatabaseDAO;
 import com.mastek.visaApplication.entities.DNADatabase;
-
-
+import com.mastek.visaApplication.entities.Gender;
 import com.mastek.visaApplication.dao.PaymentDAO;
+import com.mastek.visaApplication.dao.PersonalDetailsDAO;
 import com.mastek.visaApplication.entities.Payment;
+import com.mastek.visaApplication.entities.PersonalDetails;
+import com.mastek.visaApplication.entities.Relationship;
+import com.mastek.visaApplication.entities.Salutation;
 import com.mastek.visaApplication.services.VisaApplicationServices;
 
 
@@ -29,19 +32,62 @@ class VisaApplicationTests {
 
 	@Autowired
 	PaymentDAO payDAO;
-//
-//
-//	@Test // test add applicant details 
-//	void testAddApplicant() {
-//	PersonalDetails appd = new PersonalDetails();
-//	appd.setPassportNo(1111111111);
-//	appd.setContactLanguage("English");
-//
-//	}
+	
+	@Autowired
+	PersonalDetailsDAO perddao;
 
-	//@Test
+
+	@Test // test add applicant details 
 	void testAddApplicant() {
+	PersonalDetails appd = new PersonalDetails();
+	
+	appd.setPassportNo(1111111111);
+	appd.setContactLanguage("English");
+	appd.setApplicantSalutation(Salutation.Mr);
+	appd.setFirstName("Shujahat");
+	appd.setMiddleName("ali");
+	appd.setFamilyName("bhatti");
+	appd.setOtherNames("saj");
+	appd.setApplicantGender(Gender.Male);
+	appd.setDateOfBirth("05/02/1996");
+	appd.setCountryOfBirth("United Kingdom");
+	appd.setPlaceOfBirth("Birmingham");
+	appd.setNationality("British");
+	appd.setDoYouHaveAnyOtherNationality(false);
+	appd.setApplicantRelationship(Relationship.Married);
+	appd.setOwnershipStatusOfHome("Renting");
+	appd.setAddress("123 Coventry Road, Birmingham,CV10 9AQ");
+	appd.setHowLongHaveYouLivedAtThisAddress("5 Years");
+	appd.setIsThisYourCorrespondenceAddress(true);
+	appd.setTelephoneNumber(0123324123);
+	appd.setWhereDoYouUseThisNumber("Personal");
+	appd.setContactEmail("saj@outlook.com");
+	appd.setWhoDoesThisEmailBelongTo("myself");
+	appd.setSecondaryEmail("N/A");
+	appd.setDrivingLicense(true);
+	appd.setDoYouHaveAValidNationalIdentityCard(true);
+	appd.setPassportIssueDate("01/01/2013");
+	appd.setPassportExpiryDate("01/01/2023");
+	appd.setCountryOfNationality("United Kingdom");
+	appd.setIssuingAuthority("HM Government");
+	appd.setDoYouHaveAUkDriversLicense(true);
+	appd.setDoYouHaveAUkNationalInsuranceNumber(true);
+	appd.setNationalInsuranceNumber("SA156 7666 PA");
+	appd.setDependantFamilyName("bhatti");
+	appd.setDependantSalutation(Salutation.Dr);
+	appd.setDependantGivenName("Mo");
+	appd.setDependantRelationshipToYou("Father");
+	appd.setDependantNationalitySameAsApplicant("British");
+	appd.setDependantCountryOfNationality("United Kingdom");
+	appd.setDependantDateOfBirth("03/11/1971");
+	
+	appd = perddao.save(appd);
+	System.out.println(appd);
+	assertNotNull(appd, "applicant not added");
+
 	}
+
+	
 
 	@Test
 	void testAddPayment() {
