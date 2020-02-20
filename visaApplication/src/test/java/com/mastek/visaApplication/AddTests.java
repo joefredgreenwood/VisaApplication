51ddf11f8549eb1d555c18135219f0115674097f
joseph61288 <joseph61288@GB-LD1LPW10G019.mastek.com>
2020-02-20 13:42:40 +0000

    Changes

--- visaApplication/src/test/java/com/mastek/visaApplication/AddTests.java
+++ visaApplication/src/test/java/com/mastek/visaApplication/AddTests.java
@@ -29,10 +29,10 @@ import com.mastek.visaApplication.services.VisaApplicationServices;
 
 @SpringBootTest
 public class AddTests {
-	
+
 	@Autowired
 	VisaApplicationServices visaServices;
-	
+
 
 	@Autowired
 	DNADatabaseDAO dnadao;
@@ -47,23 +47,23 @@ public class AddTests {
 
 	@Autowired
 	ApplicationFormDAO appFormDAO;
-	
+
 	@Autowired
 	CountriesDAO couDAO;
 
 	@Autowired
 	LanguagesDAO lanDAO;
 
-	
+
 
 	@Autowired
 	ApplicationFormListeners appL;
 
 	@Autowired
 	VisaApplicationServices visaAppServices;
-	
+
 	//@Test
-		void testAddPersonalDetails() {
+	void testAddPersonalDetails() {
 		PersonalDetails perd = new PersonalDetails();
 		perd.setPassportNo(696969);
 		perd.setApplicantSalutation(Salutation.MR);
@@ -103,233 +103,237 @@ public class AddTests {
 		perd.setDependantNationalitySameAsApplicant("Italian");
 		perd.setDependantCountryOfNationality("Italy");
 		perd.setDependantDateOfBirth("02/06/1975");
-		
-		   perd = perddao.save(perd);
-		    visaAppServices.mongoDecisionMaker(perd);
-		    System.out.println(visaAppServices.mongoDecisionMaker(perd));
-		    perd.setDnaDatabaseScreeningStatus(visaAppServices.getMongoDecisionV());
-		    perd = perddao.save(perd);
-		
+
+		perd = perddao.save(perd);
+		visaAppServices.mongoDecisionMaker(perd);
+		System.out.println(visaAppServices.mongoDecisionMaker(perd));
+		perd.setDnaDatabaseScreeningStatus(visaAppServices.getMongoDecisionV());
+		perd = perddao.save(perd);
+
+	}
+
+
+
+
+
+
+
+
+	//@Test
+	void testAddApplicationForm() {
+		ApplicationForm appForm = new ApplicationForm(); 
+
+		appForm.setApplicationDate("17/02/2020");
+		appForm.setDurationOfVisa(4);
+		appForm.setSubmissionType(SubmissionType.ONLINE);
+		appForm.setPlannedArriveUK("20/05/2020");
+		appForm.setPlannedDepartUK("20/09/2020");
+		appForm.setVisaContactNumberUK(0244123453);
+		appForm.setVisaContactNumberOutsideUK(0744123453);
+		//add contact language
+		appForm.setDetailsOfVisit("Example Details of Visit");
+		appForm.setWhatYouPaidFor("Accomodation and Flights");
+		appForm.setPayAmount(1000.00);
+		appForm.setWhoWillBePayingYou("Family member");
+		appForm.setAnyoneRelyOnYouFinancially(false);
+		appForm.setReceivedAnyUKPublicFunds(false);
+		appForm.setWhereDoYouPlanToStay("With Friends");
+		appForm.setAddressOfWhereYouAreStaying("Example Address");
+		appForm.setDoYouHaveFamilyInUK(true);
+		appForm.setWhyAreTheyHelpingPay("They want to see me");
+		appForm.setHowMuchFamilyPaying(3000.00);
+		appForm.setWillAnyoneBePayingTowardsVisit(true);
+		appForm.setWhoWillBePayingTowardsVisit("Family and Partner");
+		appForm.setMonthlySpend(700.00);
+		appForm.setPlannedUKSpending(2000.00);
+		appForm.setOtherIncomeOrSavings(true);
+		appForm.setDiscribeYourJob("Job Description");
+		appForm.setSalaryAfterTax(20000.00);
+		appForm.setJobTitle("Builder");
+		appForm.setEmployersName("Example Company");
+		appForm.setEmployersAddress("Employers Address");
+		appForm.setEmployersPhoneNumber(0276453213);
+		appForm.setStartedWorkingForEmployer("01/01/2004");
+		appForm.setEmploymentStatus(EmploymentStatus.FULLTIME);
+		appForm.setHadMedicalTreatmentInUK(false);
+		appForm.setHaveYouEnteredUKIllegally(false);
+		appForm.setHaveYouStayedBeyondYourVisa(false);
+		appForm.setHaveYouBreachedVisaConditions(false);
+		appForm.setHaveYouReceivedPublicFundsWithoutPermission(false);
+		appForm.setHaveYouGivenFalseInfoOnVisa(false);
+		appForm.setHaveYouBeenToUKInLast10Years(true);
+		//countries question 
+		appForm.setRefusedVisa(true);
+		if (appForm.getRefusedVisa()) {
+			appForm.setRefusedVisaReason("Add Reason");
 		}
-		
-		
-		
-		
-		
-	
-		
-		
-		//@Test
-		void testAddApplicationForm() {
-			ApplicationForm appForm = new ApplicationForm(); 
+		else {
+			appForm.setRefusedVisaReason(null);
+		}
+		appForm.setRefusedEntryAtBorder(false);
+		if (appForm.getRefusedEntryAtBorder()) {
+			appForm.setRefusedEntryAtBorderReason("Add Reason");
+		}
+		else {
+			appForm.setRefusedEntryAtBorderReason(null);
+		}
+		appForm.setRefusedPermissionToStay(true);
+		if (appForm.getRefusedPermissionToStay()) {
+			appForm.setRefusedPermissionToStayReason("Add Reason");	
+		}
+		else {
+			appForm.setRefusedPermissionToStayReason(null);
+		}
+		appForm.setRefusedAsylum(false);
+		if (appForm.getRefusedAsylum()) {
+			appForm.setRefusedAsylumReason("Add Reason");	
+		}
+		else {
+			appForm.setRefusedAsylumReason(null);
+		}
+		appForm.setDeportedFromCountry(true);
+		if(appForm.getDeportedFromCountry()) {
+			appForm.setDeportedFromCountryReason("Add Reason");
+		}
+		else {
+			appForm.setDeportedFromCountryReason(null);
+		}
+		appForm.setRemovedFromCountry(false);
+		if (appForm.getRemovedFromCountry()) {
+			appForm.setRemovedFromCountryReason("Add Reason");
+		} 
+		else {
+			appForm.setRemovedFromCountryReason(null);
+		}
+		appForm.setRequiredToLeaveCountry(true);
+		if (appForm.getRequiredToLeaveCountry()) {
+			appForm.setRequiredToLeaveCountryReason("Add Reason");
+		}
+		else {
+			appForm.setRequiredToLeaveCountryReason(null);
+		}
+		appForm.setExcludedOrBannedFromEntry(false);
+		if (appForm.getExcludedOrBannedFromEntry()) {
+			appForm.setExcludedOrBannedFromEntryReason("Add Reason");
+		}
+		else {
+			appForm.setExcludedOrBannedFromEntryReason(null);
+		}
+		appForm.setConfirmReadInfoOnTerrorism(true);
+		appForm.setHaveYouJustifiedOrEncouragedTerrorism(false);
+		if (appForm.getHaveYouJustifiedOrEncouragedTerrorism()) {
+			appForm.setHaveYouJustifiedOrEncouragedTerrorismReason("Add Reason");
+		}
+		else { 
+			appForm.setHaveYouJustifiedOrEncouragedTerrorismReason(null);
+		}
+		appForm.setHaveYouBeenAMemberOfTerroristOrginisation(false);
+		if (appForm.getHaveYouBeenAMemberOfTerroristOrginisation()) {
+			appForm.setHaveYouBeenAMemberOfTerroristOrginisationReason("Add Reason");
+		}
+		else {
+			appForm.setHaveYouBeenAMemberOfTerroristOrginisationReason(null);	
+		}
+		appForm.setHaveYouSupportedTerrorism(false); 
+		if (appForm.getHaveYouSupportedTerrorism()) {
+			appForm.setHaveYouSupportedTerrorismReason("Add Reason");
+		}
+		else {
+			appForm.setHaveYouSupportedTerrorismReason(null);
+		}
+		appForm.setHaveYouBeenSuspectedOrInvolvedInWarCrimes(true);
+		if (appForm.getHaveYouBeenSuspectedOrInvolvedInWarCrimes()) {
+			appForm.setHaveYouBeenSuspectedOrInvolvedInWarCrimesReason("Add Reason");
+		}
+		else {
+			appForm.setHaveYouBeenSuspectedOrInvolvedInWarCrimesReason(null);
+		}
+		appForm.setAnyOtherInfoAboutCharacter(false);
+		if (appForm.getAnyOtherInfoAboutCharacter()) {
+			appForm.setAnyOtherInfoAboutCharacterReason("Add Reason");
+		}
+		else {
+			appForm.setAnyOtherInfoAboutCharacterReason(null);
+		}
+		appForm.setAnyOtherActivitiesShowNotGoodPerson(false);
+		if (appForm.getAnyOtherActivitiesShowNotGoodPerson()) {
+			appForm.setAnyOtherActivitiesShowNotGoodPersonReason("Add Reason");
+		}
+		else {
+			appForm.setAnyOtherActivitiesShowNotGoodPersonReason(null);
+		}
+		appForm.setWorkedForOrginisationDangerousToUKOrAllies(false);
+		if (appForm.getWorkedForOrginisationDangerousToUKOrAllies()) {
+			appForm.setWorkedForOrginisationDangerousToUKOrAlliesReason("Add Reason");
+		}
+		else {
+			appForm.setWorkedForOrginisationDangerousToUKOrAlliesReason(null);
+		}
+		appForm.setHaveYouExpressedAnyExtremistViews(false);
+		if (appForm.getHaveYouExpressedAnyExtremistViews()) {
+			appForm.setHaveYouExpressedAnyExtremistViewsReason("Add Reason");
+		}
+		else {
+			appForm.setHaveYouExpressedAnyExtremistViewsReason(null);
+		}
+		appForm.setHaveYouSupportedExtreamistOrgisisation(false);
+		if (appForm.getHaveYouSupportedExtreamistOrgisisation()) {
+			appForm.setHaveYouSupportedExtreamistOrgisisationReason("Add Reason");
+		}
+		else {
+			appForm.setHaveYouSupportedExtreamistOrgisisationReason(null);
+		}
 
-			appForm.setApplicationDate("17/02/2020");
-			appForm.setDurationOfVisa(4);
-			appForm.setSubmissionType(SubmissionType.ONLINE);
-			appForm.setPlannedArriveUK("20/05/2020");
-			appForm.setPlannedDepartUK("20/09/2020");
-			appForm.setVisaContactNumberUK(0244123453);
-			appForm.setVisaContactNumberOutsideUK(0744123453);
-			//add contact language
-			appForm.setDetailsOfVisit("Example Details of Visit");
-			appForm.setWhatYouPaidFor("Accomodation and Flights");
-			appForm.setPayAmount(1000.00);
-			appForm.setWhoWillBePayingYou("Family member");
-			appForm.setAnyoneRelyOnYouFinancially(false);
-			appForm.setReceivedAnyUKPublicFunds(false);
-			appForm.setWhereDoYouPlanToStay("With Friends");
-			appForm.setAddressOfWhereYouAreStaying("Example Address");
-			appForm.setDoYouHaveFamilyInUK(true);
-			appForm.setWhyAreTheyHelpingPay("They want to see me");
-			appForm.setHowMuchFamilyPaying(3000.00);
-			appForm.setWillAnyoneBePayingTowardsVisit(true);
-			appForm.setWhoWillBePayingTowardsVisit("Family and Partner");
-			appForm.setMonthlySpend(700.00);
-			appForm.setPlannedUKSpending(2000.00);
-			appForm.setOtherIncomeOrSavings(true);
-			appForm.setDiscribeYourJob("Job Description");
-			appForm.setSalaryAfterTax(20000.00);
-			appForm.setJobTitle("Builder");
-			appForm.setEmployersName("Example Company");
-			appForm.setEmployersAddress("Employers Address");
-			appForm.setEmployersPhoneNumber(0276453213);
-			appForm.setStartedWorkingForEmployer("01/01/2004");
-			appForm.setEmploymentStatus(EmploymentStatus.FULLTIME);
-			appForm.setHadMedicalTreatmentInUK(false);
-			appForm.setHaveYouEnteredUKIllegally(false);
-			appForm.setHaveYouStayedBeyondYourVisa(false);
-			appForm.setHaveYouBreachedVisaConditions(false);
-			appForm.setHaveYouReceivedPublicFundsWithoutPermission(false);
-			appForm.setHaveYouGivenFalseInfoOnVisa(false);
-			appForm.setHaveYouBeenToUKInLast10Years(true);
-			//countries question 
-			appForm.setRefusedVisa(true);
-			if (appForm.getRefusedVisa()) {
-				appForm.setRefusedVisaReason("Add Reason");
-			}
-			else {
-					appForm.setRefusedVisaReason(null);
-				}
-			appForm.setRefusedEntryAtBorder(false);
-			if (appForm.getRefusedEntryAtBorder()) {
-				appForm.setRefusedEntryAtBorderReason("Add Reason");
-			}
-			else {
-					appForm.setRefusedEntryAtBorderReason(null);
-				}
-			appForm.setRefusedPermissionToStay(true);
-			if (appForm.getRefusedPermissionToStay()) {
-				appForm.setRefusedPermissionToStayReason("Add Reason");	
-			}
-			else {
-				appForm.setRefusedPermissionToStayReason(null);
-			}
-			appForm.setRefusedAsylum(false);
-			if (appForm.getRefusedAsylum()) {
-				appForm.setRefusedAsylumReason("Add Reason");	
-			}
-			else {
-				appForm.setRefusedAsylumReason(null);
-			}
-			appForm.setDeportedFromCountry(true);
-			if(appForm.getDeportedFromCountry()) {
-				appForm.setDeportedFromCountryReason("Add Reason");
-			}
-			else {
-				appForm.setDeportedFromCountryReason(null);
-			}
-			appForm.setRemovedFromCountry(false);
-			if (appForm.getRemovedFromCountry()) {
-				appForm.setRemovedFromCountryReason("Add Reason");
-			} 
-			else {
-				appForm.setRemovedFromCountryReason(null);
-			}
-			appForm.setRequiredToLeaveCountry(true);
-			if (appForm.getRequiredToLeaveCountry()) {
-				appForm.setRequiredToLeaveCountryReason("Add Reason");
-			}
-			else {
-				appForm.setRequiredToLeaveCountryReason(null);
-			}
-			appForm.setExcludedOrBannedFromEntry(false);
-			if (appForm.getExcludedOrBannedFromEntry()) {
-				appForm.setExcludedOrBannedFromEntryReason("Add Reason");
-			}
-			else {
-				appForm.setExcludedOrBannedFromEntryReason(null);
-			}
-			appForm.setConfirmReadInfoOnTerrorism(true);
-			appForm.setHaveYouJustifiedOrEncouragedTerrorism(false);
-			if (appForm.getHaveYouJustifiedOrEncouragedTerrorism()) {
-				appForm.setHaveYouJustifiedOrEncouragedTerrorismReason("Add Reason");
-			}
-			else { 
-				appForm.setHaveYouJustifiedOrEncouragedTerrorismReason(null);
-			}
-			appForm.setHaveYouBeenAMemberOfTerroristOrginisation(false);
-			if (appForm.getHaveYouBeenAMemberOfTerroristOrginisation()) {
-				appForm.setHaveYouBeenAMemberOfTerroristOrginisationReason("Add Reason");
-			}
-			else {
-				appForm.setHaveYouBeenAMemberOfTerroristOrginisationReason(null);	
-			}
-			appForm.setHaveYouSupportedTerrorism(false); 
-			if (appForm.getHaveYouSupportedTerrorism()) {
-				appForm.setHaveYouSupportedTerrorismReason("Add Reason");
-			}
-			else {
-				appForm.setHaveYouSupportedTerrorismReason(null);
-			}
-			appForm.setHaveYouBeenSuspectedOrInvolvedInWarCrimes(true);
-			if (appForm.getHaveYouBeenSuspectedOrInvolvedInWarCrimes()) {
-				appForm.setHaveYouBeenSuspectedOrInvolvedInWarCrimesReason("Add Reason");
-			}
-			else {
-				appForm.setHaveYouBeenSuspectedOrInvolvedInWarCrimesReason(null);
-			}
-			appForm.setAnyOtherInfoAboutCharacter(false);
-			if (appForm.getAnyOtherInfoAboutCharacter()) {
-				appForm.setAnyOtherInfoAboutCharacterReason("Add Reason");
-			}
-			else {
-				appForm.setAnyOtherInfoAboutCharacterReason(null);
-			}
-			appForm.setAnyOtherActivitiesShowNotGoodPerson(false);
-			if (appForm.getAnyOtherActivitiesShowNotGoodPerson()) {
-				appForm.setAnyOtherActivitiesShowNotGoodPersonReason("Add Reason");
-			}
-			else {
-				appForm.setAnyOtherActivitiesShowNotGoodPersonReason(null);
-			}
-			appForm.setWorkedForOrginisationDangerousToUKOrAllies(false);
-			if (appForm.getWorkedForOrginisationDangerousToUKOrAllies()) {
-				appForm.setWorkedForOrginisationDangerousToUKOrAlliesReason("Add Reason");
-			}
-			else {
-				appForm.setWorkedForOrginisationDangerousToUKOrAlliesReason(null);
-			}
-			appForm.setHaveYouExpressedAnyExtremistViews(false);
-			if (appForm.getHaveYouExpressedAnyExtremistViews()) {
-				appForm.setHaveYouExpressedAnyExtremistViewsReason("Add Reason");
-			}
-			else {
-				appForm.setHaveYouExpressedAnyExtremistViewsReason(null);
-			}
-			appForm.setHaveYouSupportedExtreamistOrgisisation(false);
-			if (appForm.getHaveYouSupportedExtreamistOrgisisation()) {
-				appForm.setHaveYouSupportedExtreamistOrgisisationReason("Add Reason");
-			}
-			else {
-				appForm.setHaveYouSupportedExtreamistOrgisisationReason(null);
-			}
-
-			appForm = appFormDAO.save(appForm);
-			System.out.println(appForm);
-			
+		appForm = appFormDAO.save(appForm);
+		visaAppServices.DecisionMaker(appForm);
+		System.out.println(visaAppServices.DecisionMaker(appForm));
+		appForm.setAppQuestionsStatus(visaAppServices.getDecision());
+		System.out.println(appForm);
+		appFormDAO.save(appForm);
 
 
-			}
-		
-		
-		
-		//@Test
-				void testAddPayment() {
-				Payment pay = new Payment();
-				pay.setTotalFee(33.50);
 
-				pay = payDAO.save(pay);
-				System.out.println(pay);
-				assertNotNull(pay, "Payment not Added");
-				}
-				
-				
-				
-				//@Test
-				void testAddCountry() {
-					Countries cou = new Countries();
-					cou.setCountryName("Brazil");
-
-					cou = couDAO.save(cou);
-					System.out.println(cou);
-
-				}
-				
-				
-				
-				//@Test
-				void testAddLanguages() {
-					Languages lan = new Languages();
-					lan.setLanguageName("Russian");
+	}
 
 
-					lan = lanDAO.save(lan);
-					System.out.println(lan);
 
-				}
-				
-				
-				
-				
+	//@Test
+	void testAddPayment() {
+		Payment pay = new Payment();
+		pay.setTotalFee(33.50);
+
+		pay = payDAO.save(pay);
+		System.out.println(pay);
+		assertNotNull(pay, "Payment not Added");
+	}
+
+
+
+	//@Test
+	void testAddCountry() {
+		Countries cou = new Countries();
+		cou.setCountryName("Brazil");
+
+		cou = couDAO.save(cou);
+		System.out.println(cou);
+
+	}
+
+
+
+	//@Test
+	void testAddLanguages() {
+		Languages lan = new Languages();
+		lan.setLanguageName("Russian");
+
+
+		lan = lanDAO.save(lan);
+		System.out.println(lan);
+
+	}
+
+
+
+
 
 }
